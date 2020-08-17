3ac206fcd4b9b85245062a28f09520a8be573b8f
Tyler Degand <tylerdegand@gmail.com>
2020-08-17 14:44:37 -0500

    Made some changes, Started pulling in photo Data

--- react-gallery/src/App.js
+++ react-gallery/src/App.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 import './css/index.css'
+import axios from 'axios';
 import SearchForm from './components/Search';
 import Nav from './components/Nav'
 import Router from './components/Router';
 
 
 class App extends Component{
+
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    };
+  }
+
+  componentDidMount(){
+    axios.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=622ac78a6d9e7d1350206211b652a6af&tags=Cats&text=Cats&per_page=24&format=json&nojsoncallback=1')
+    .then(response => {
+      console.log(response.data.photos)
+      this.setState({
+        images: response.data.photos
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
   render() {
     return (
       <div className="App">
