5e1e218803ca09e58a5ff6b337108d1cd3e8926e
Tyler Degand <tylerdegand@gmail.com>
2020-08-17 08:39:38 -0500

    Started adding components

--- react-gallery/src/App.js
+++ react-gallery/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './css/index.css'
+import SearchForm from './components/Search';
+import Nav from './components/Nav'
 
 class App extends Component{
   render() {
@@ -7,6 +9,8 @@ class App extends Component{
       <div className="App">
         <div class="container">  
         <h1>All items will go here</h1>
+        <SearchForm />
+        <Nav />
         </div>
       </div>
     );
--- react-gallery/src/components/Nav.js
+++ react-gallery/src/components/Nav.js
@@ -1,1 +1,18 @@
 //stateless component
+import React from 'react';
+
+class Nav extends React.Component{
+    render() {
+        return(
+            <nav class="main-nav">
+                <ul>
+                <li><a href='#'>Cats</a></li>
+                <li><a href='#'>Dogs</a></li>
+                <li><a href='#'>Computers</a></li>
+                </ul>
+            </nav>
+                )
+    }
+}
+
+export default Nav;
